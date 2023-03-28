4a489872661951c4e5c0b4522abe27df5be78a0a
Devin Singh <drsingh2518@icloud.com>
2023-03-28 08:15:04 -0400

    Parallelize parsing
    - Instead of parsing whole file at once, we break it up by line and
      parse those
    - Significant speedup (~40s to ~5s on asicfab)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include "analyzer.hpp"
 #include "parser.hpp"
-#include "src/analyzer.hpp"
+#include <execution>
+#include <fstream>
+#include <iostream>
+#include <mutex>
 #include <unistd.h>
 
 int main(int argc, const char **argv) {
@@ -23,27 +27,36 @@ int main(int argc, const char **argv) {
         return EXIT_FAILURE;
     }
     parser_t p = parser_t();
-
-    mpc_result_t r;
-    // TODO: Extremely slow. Can probably multithread this
-    // Read line by line then use p.trace
-    if (mpc_parse_contents(trace_file, p.trace_file, &r)) {
-        mpc_ast_t *ast = (mpc_ast_t *)r.output;
-        std::vector<std::string> instrs;
-        std::vector<std::string> pcs;
-        for (int i = 0; i < ast->children_num; i++) {
-            instrs.push_back(
-                ast->children[i]->children[INSTRUCTION_IDX]->contents);
-            pcs.push_back(ast->children[i]->children[ADDRESS_IDX]->contents);
-        }
-        Analyzer analyzer(instrs, pcs, binary_file);
-        analyzer.analyze();
-        mpc_ast_delete((mpc_ast_t *)r.output);
-    } else {
-        printf("Could not parse file %s: %s", trace_file,
-               mpc_err_string(r.error));
-        mpc_err_delete(r.error);
+    std::ifstream trace_if(trace_file);
+    std::string line;
+    std::vector<std::string> lines;
+    while (std::getline(trace_if, line)) {
+        lines.push_back(line);
     }
+
+    std::vector<std::string> instrs;
+    std::vector<std::string> pcs;
+    std::mutex m;
+    std::for_each(
+        std::execution::par, lines.begin(), lines.end(),
+        [&](std::string_view s) {
+            mpc_result_t r;
+            if (mpc_parse("trace", s.data(), p.trace, &r)) {
+                mpc_ast_t *ast = (mpc_ast_t *)r.output;
+                {
+                    std::lock_guard lock(m);
+                    instrs.push_back(ast->children[INSTRUCTION_IDX]->contents);
+                    pcs.push_back(ast->children[ADDRESS_IDX]->contents);
+                }
+                mpc_ast_delete((mpc_ast_t *)r.output);
+            } else {
+                printf("Could not parse trace %s: %s", s.data(),
+                       mpc_err_string(r.error));
+                mpc_err_delete(r.error);
+            }
+        });
+    Analyzer analyzer(instrs, pcs, binary_file);
+    analyzer.analyze();
 
     return EXIT_SUCCESS;
 }
